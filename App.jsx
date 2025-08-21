13606eb7fc00f242c6fd00c3cccdef5533971dd5
DominicDev <s0l0estudi4r@gmail.com>
2025-08-21 14:14:51 +0200

    v0.0.2

---
+++ App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { setup, tw } from "twind";
+
+setup({});
+
+const App = () => {
+  return (
+    <div
+      className={tw`
+        h-screen w-full
+        bg-[url('assets/bg.svg')]
+        bg-cover bg-top
+        overflow-hidden
+      `}
+    ></div>
+  );
+};
+
+const rootElement = document.createElement("div");
+document.body.appendChild(rootElement);
+const root = createRoot(rootElement);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
